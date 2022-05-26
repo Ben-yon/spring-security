b339f30a424c25a650da894f5586bf6257bd21d1
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-05-26 15:52:06 +0000

    add payload classes

--- src/main/java/io/benyon/jpajwt/SecurityConfiguration.java
+++ src/main/java/io/benyon/jpajwt/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package io.benyon.jpajwt;
 
+import io.benyon.jpajwt.jwt.AuthEntryPointJwt;
+import io.benyon.jpajwt.jwt.AuthTokenFilter;
 import io.benyon.jpajwt.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
