d332c220ecc3221ddb10925cee7f326bb1aaab52
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-05-26 12:32:58 +0000

    add various files for th project

---
+++ src/main/java/io/benyon/jpajwt/models/models/models/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package io.benyon.jpajwt.models.models.models.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+  Boolean existsByUsername(String username);
+  Boolean existsByEmail(String email);
+}
---
+++ src/test/java/io/benyon/jpajwt/JpaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.benyon.jpajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
